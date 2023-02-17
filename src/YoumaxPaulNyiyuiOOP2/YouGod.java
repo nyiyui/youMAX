7f921f7d1b6481111d7553239781286137c447f2
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-02-17 17:55:10 -0500

    added more attributes and methods to yougod and youheart

--- src/YoumaxPaulNyiyuiOOP2/YouGod.java
+++ src/YoumaxPaulNyiyuiOOP2/YouGod.java
@@ -17,10 +17,83 @@ public class YouGod {
 
     public YouGod() {
         name = "YouGOD";
-        youHeart = new YouHeart(); //TODO: change constructor once YouHeart has all its variable and a proper constructor
-        height = 150;
+        youHeart = new YouHeart();
+        height = 100;
         thoughts = new ArrayList<String>();
         immortal = true;
     }
 
+    public YouGod(String name, YouHeart youHeart, int height, boolean immortal) {
+        this.name = name;
+        this.youHeart = youHeart;
+        this.height = height;
+        this.thoughts = new ArrayList<>();
+        this.immortal = immortal;
+    }
+    public void beginCreation(){
+        if(youHeart.isLonely()){
+            youHeart.setLonely(false);
+            youHeart.setEmotion("Empowered");
+            System.out.println(name+" has decided to enlighten the Youniverse!");
+        }
+    }
+    public void thinkOf(String thought, String emotion){
+        thoughts.add(thought);
+        youHeart.setEmotion(emotion);
+        System.out.println(name + " just thought of something!");
+        System.out.println("\t--> \""+thought+"\"");
+        System.out.println("Now "+name+" feels "+emotion+"!");
+    }
+    public void die(){
+        if(immortal){
+            System.out.println(name+" never dies! Transcending to the next Youniverse...");
+        }
+        else{
+            youHeart.die();
+            System.out.println(name+" has lived a long healthy life. :(");
+        }
+    }
+    public void grow(int growth){
+        height += growth;
+        youHeart.setHp(youHeart.getHp() + growth/2.0);
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public YouHeart getYouHeart() {
+        return youHeart;
+    }
+
+    public void setYouHeart(YouHeart youHeart) {
+        this.youHeart = youHeart;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public ArrayList<String> getThoughts() {
+        return thoughts;
+    }
+
+    public void setThoughts(ArrayList<String> thoughts) {
+        this.thoughts = thoughts;
+    }
+
+    public boolean isImmortal() {
+        return immortal;
+    }
+
+    public void setImmortal(boolean immortal) {
+        this.immortal = immortal;
+    }
 }
--- src/YoumaxPaulNyiyuiOOP2/YouHeart.java
+++ src/YoumaxPaulNyiyuiOOP2/YouHeart.java
@@ -8,6 +8,52 @@ package YoumaxPaulNyiyuiOOP2;
 
 public class YouHeart {
     private int bpm;
-    private boolean inLove; //TODO: lmao is this a good variable
+    private boolean lonely;
+    private double hp;
+    private String emotion;
 
+    public YouHeart() {
+        bpm = 100;
+        lonely = true;
+        hp = 100;
+        emotion = "Melancholy";
+    }
+
+    public void die(){
+        bpm = 0;
+        emotion = "Content";
+        lonely = true;
+        hp = 0;
+    }
+    public int getBpm() {
+        return bpm;
+    }
+
+    public void setBpm(int bpm) {
+        this.bpm = bpm;
+    }
+
+    public boolean isLonely() {
+        return lonely;
+    }
+
+    public void setLonely(boolean lonely) {
+        this.lonely = lonely;
+    }
+
+    public double getHp() {
+        return hp;
+    }
+
+    public void setHp(double hp) {
+        this.hp = hp;
+    }
+
+    public String getEmotion() {
+        return emotion;
+    }
+
+    public void setEmotion(String emotion) {
+        this.emotion = emotion;
+    }
 }
