66ef367006c7ca0923f0ae97916e4057386e2ce9
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-02-17 15:41:59 -0500

    added more attributes to YouGod and created YouHeart

--- src/YoumaxPaulNyiyuiOOP2/YouGod.java
+++ src/YoumaxPaulNyiyuiOOP2/YouGod.java
@@ -1,8 +1,26 @@
-
+/**
+ * Names: Youmin Lee, Ivy Zhuang, Ken Shibata
+ * Teacher: Ms. Krasteva
+ * Date:
+ * Purpose:
+ */
 package YoumaxPaulNyiyuiOOP2;
 
+import java.util.ArrayList;
+
 public class YouGod {
-    public String name;
-    private int youHeartBPM;
-    
+    private String name;
+    private YouHeart youHeart;
+    private int height;
+    private ArrayList<String> thoughts;
+    private boolean immortal;
+
+    public YouGod() {
+        name = "YouGOD";
+        youHeart = new YouHeart(); //TODO: change constructor once YouHeart has all its variable and a proper constructor
+        height = 150;
+        thoughts = new ArrayList<String>();
+        immortal = true;
+    }
+
 }
---
+++ src/YoumaxPaulNyiyuiOOP2/YouHeart.java
@@ -0,0 +1,13 @@
+/**
+ * Names: Youmin Lee, Ivy Zhuang, Ken Shibata
+ * Teacher: Ms. Krasteva
+ * Date:
+ * Purpose:
+ */
+package YoumaxPaulNyiyuiOOP2;
+
+public class YouHeart {
+    private int bpm;
+    private boolean inLove; //TODO: lmao is this a good variable
+
+}
