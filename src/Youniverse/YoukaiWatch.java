bcf8fe30e9e0e60fd91b0af03077d37311b468f9
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-02-20 17:35:15 -0500

    Update YoukaiWatch.java

--- src/Youniverse/YoukaiWatch.java
+++ src/Youniverse/YoukaiWatch.java
@@ -1,8 +1,17 @@
+/**
+ * Names: Ivy Zhuang, Ken Shibata, Youmin Lee
+ * Teacher: Ms. Krasteva
+ * Date: Feb
+ * Purpose: YouKaiWatch Class, a watch that every youperson owns which is used to summon the youkais it stores
+ */
 package Youniverse;
+
+import java.util.ArrayList;
 
 public class YoukaiWatch extends YouGod{
     //TODO: constructor, methods, attributes, comments
-    public static void call() {
+    private ArrayList<Youkai> youkais;
+    public void call() {
         for (Youkai y : youkais) {
             y.helpThePeople();
         }
