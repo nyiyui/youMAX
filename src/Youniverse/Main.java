7bb76a7860a00d412b633cc27efbb73834f62a93
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-02-20 17:41:39 -0500

    Create Main.java

---
+++ src/Youniverse/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Names: Ivy Zhuang, Ken Shibata, Youmin Lee
+ * Teacher: Ms. Krasteva
+ * Date: Feb 21, 2023
+ * Purpose: Driver class, tests out classes and their instance methods
+ */
+package Youniverse;
+
+public class Main {
+    public static void main(String[] args) {
+        YouGod youGod = new YouGod();
+        youGod.beginCreation();
+    }
+}
