e2453e76eb7d403f5de8d06dc72058db1aa6dfe9
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-02-20 17:35:12 -0500

    Update Youkai.java

--- src/Youniverse/Youkai.java
+++ src/Youniverse/Youkai.java
@@ -1,16 +1,25 @@
+/**
+ * Names: Ivy Zhuang, Ken Shibata, Youmin Lee
+ * Teacher: Ms. Krasteva
+ * Date: Feb 20, 2023
+ * Purpose: Youkai class, youkai are mythical younimals with the power to help the youpeople
+ */
 package Youniverse;
 
+import java.util.ArrayList;
 
 public class Youkai extends Younimal{
-    Youkai() {
-        super(); //TODO: fix constructor here
+    //TODO: add more attributes and methods, javadoc comments, etc.
+    protected ArrayList<Youkai> youkais;
+    public Youkai(String name, int height, double speed) {
+        super(name, height, true, speed);
         youkais.add(this);
     }
 
     void helpThePeople() {
-        for (YouPerson p : people) {
-            help(p);
-        }
+//        for (YouPerson p : people) {
+//            help(p);
+//        }
     }
 
     void help(YouPerson p) {
