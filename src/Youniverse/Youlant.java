c1bc68721e04631636b80f24dd899ea536ed55de
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-02-21 01:26:32 -0500

    youlant and youmin are complete :D

--- src/Youniverse/Youlant.java
+++ src/Youniverse/Youlant.java
@@ -1,5 +1,111 @@
+/**
+ * Names: Ivy Zhuang, Ken Shibata, Youmin Lee
+ * Teacher: Ms. Krasteva
+ * Date: Feb 21, 2023
+ * Purpose: Youlant class. Youlants provide sustenance for all mortal beings in the Youniverse
+ */
 package Youniverse;
 
 public class Youlant extends Youmin {
-    //TODO: once Youmin class is done, start coding this class
+    /**
+     * youflowers = number of youflowers Youlant has
+     * youleaves = number of youleaves Youlant has
+     * nutrients = nutritional value of Youlant
+     */
+    private int youflowers;
+    private int youleaves;
+    private int nutrients;
+
+    /**
+     * constructor for Youlant, uses Youmin superconstructor
+     * initially has no youflowers and nutrients is based on number of youleaves
+     *
+     * @param name     name of Youlant
+     * @param height   how tall Youlant is in centimetres
+     * @param sunlight how much sunlight Youlant is absorbing
+     * @param biome what environment Youlant lives in
+     */
+    public Youlant(String name, int height, int sunlight, String biome, int youleaves) {
+        super(name, height, false, sunlight, biome); //youlants aren't immortal unfortunately :(
+        this.youflowers = 0;
+        this.youleaves = youleaves;
+        this.nutrients = youleaves * 4;
+    }
+    public void bloom(){
+        if(youflowers == 0 || getSunlight() < 50){
+            System.out.println(getName()+" can't bloom any youflowers right now :(");
+            return;
+        }
+        getYouHeart().setEmotion("Elated");
+        getYouHeart().setHp(getYouHeart().getHp() + youflowers * 10);
+        System.out.println(getName()+" has bloomed in the spring air with a feeling of elatedness :D");
+        System.out.println(getName()+" is feeling healthier, look their HP is "+getYouHeart().getHp()+"!");
+    }
+    /**
+     * Youlants uses the sunlight to grow :relieved:
+     * youlants also can grow youflowers. >:D youlant also grows more leaves and gets more nutrients
+     *
+     * @param growth number of centimetres YouMin could grow if there was 100% sunlight
+     */
+    @Override
+    public void grow(int growth) {
+        super.grow(growth);
+        youleaves += growth / 4;
+        nutrients += growth;
+        if(getHeight() > 100){
+            youflowers++;
+            System.out.println("Is that a little youflower that "+getName()+" has grown? So cute!");
+            System.out.println("Now "+getName()+" has "+youflowers+" youflowers");
+            return;
+        }
+        System.out.println("Unfortunately, "+getName()+" hasn't grown enough to grow any youflowers. :(");
+    }
+
+    /**
+     * accessor method for youflowers
+     * @return number of youflowers Youlant has
+     */
+    public int getYouflowers() {
+        return youflowers;
+    }
+
+    /**
+     * mutator method for youflowers
+     * @param youflowers new number of youflowers Youlant has
+     */
+    public void setYouflowers(int youflowers) {
+        this.youflowers = youflowers;
+    }
+
+    /**
+     * accessor method for youleaves
+     * @return number of youleaves Youlant has
+     */
+    public int getYouleaves() {
+        return youleaves;
+    }
+
+    /**
+     * mutator method for youleaves
+     * @param youleaves new number of youleaves
+     */
+    public void setYouleaves(int youleaves) {
+        this.youleaves = youleaves;
+    }
+
+    /**
+     * accessor method for nutrients
+     * @return Youlant's nutritional value
+     */
+    public int getNutrients() {
+        return nutrients;
+    }
+
+    /**
+     * mutator method for nutrients
+     * @param nutrients new nutritional value Youlant has
+     */
+    public void setNutrients(int nutrients) {
+        this.nutrients = nutrients;
+    }
 }
--- src/Youniverse/Youmin.java
+++ src/Youniverse/Youmin.java
@@ -17,16 +17,92 @@ public class Youmin extends YouGod {
     /**
      * 1st constructor for Youmin, Takes no parameters and sets all attributes to default values.
      */
-    Youmin() {
-        this.setName("Youmin");
+    public Youmin() {
+        this.setName("YouMin");
         this.setHeight(100);
         this.setImmortal(true);
         this.sunlight = 60;
         this.biome = "Youpical Rainforest";
     }
 
+    /**
+     * 2nd constructor for Youmin, used when one doesn't want to use the default values
+     *
+     * @param name     name of Youmin
+     * @param height   how tall Youmin is in centimetres
+     * @param immortal whether or not Youmin is immortal
+     */
+    public Youmin(String name, int height, boolean immortal, int sunlight, String biome) {
+        super(name, height, immortal);
+        this.sunlight = sunlight;
+        this.biome = biome;
+    }
 
+    /**
+     * Youmin uses the sunlight to grow :relieved:
+     *
+     * @param growth number of centimetres YouMin could grow if there was 100% sunlight
+     */
+    @Override
+    public void grow(int growth) {
+        int realGrowth = getHeight() + (int) (growth * sunlight / 100.0);
+        this.setHeight(realGrowth);
+        this.getYouHeart().setHp(realGrowth / 2.0);
+        System.out.println(getName()+" has grown "+realGrowth+" cm!");
+        System.out.println("Current HP: "+this.getYouHeart().getHp());
+    }
 
+    /**
+     * if oneself is immortal, then one will simply continue being a menace >:)
+     * else, youmin becomes a part of the soil once more. :(((
+     */
+    @Override
+    public void die() {
+        if(isImmortal()){
+            System.out.println(getName()+" is not affected by death >:)");
+        }
+        else {
+            System.out.println(getName()+" will become a part of the soil. :(");
+        }
+    }
+
+    /**
+     * if Youmin changes where she takes root, her biome, sunlight, and emotions all change. moving away is a big deal yk
+     * @param destination Youmin's new biome/home.
+     */
+    public void uproot(String destination){
+        biome = destination;
+        this.getYouHeart().setEmotion("Nostalgic"); //bc shes moving far away from home :pensive:
+        if(destination.equals("YouDesert") || destination.equals("Yougrassland")){
+            sunlight = 100;
+        }
+        else if(destination.equals("Youndra")){
+            sunlight = 40;
+        }
+        else{
+            sunlight = 60;
+        }
+        System.out.println(getName()+" now lives in the "+biome+", where she gets "+sunlight+"% of sunlight.");
+    }
+
+    /**
+     * even if Youmin can't walk like Youmax, she too can invoke her powers ;D
+     */
+    @Override
+    public void beginCreation() {
+        if(isImmortal()){
+            this.getYouHeart().setEmotion("Empowered");
+            System.out.println(getName()+" is going to create new companions, the Youlants!");
+        }
+        else{
+            System.out.println("??? "+getName()+" isn't that powerful? :>");
+        }
+    }
+
+    /**
+     * accessor method for Youmin's sunlight
+     * @return how much sunlight Youmin is absorbing
+     */
     public int getSunlight() {
         return sunlight;
     }
@@ -53,9 +129,5 @@ public class Youmin extends YouGod {
      */
     public void setBiome(String biome) {
         this.biome = biome;
-    }
-
-    public void makeYoulants() {
-        // TODO: impl
     }
 }
