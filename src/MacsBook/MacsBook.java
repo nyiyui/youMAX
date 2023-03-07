2a6563cd5a5aae50caea2e09bc023089cddc2907
nyiyui <+@nyiyui.ca>
2023-03-07 14:32:05 -0500

    MacsBook: done maybe

--- src/MacsBook/MacsBook.java
+++ src/MacsBook/MacsBook.java
@@ -1,9 +1,4 @@
 package MacsBook;
-
-import Pokemon_Rating.AllDataScreen;
-import Pokemon_Rating.PremiseScreen;
-import javafx.css.CssParser;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,10 +9,15 @@ public class MacsBook {
     private ArrayList<Double> tMarks = new ArrayList<>();
     private ArrayList<Double> fMarks = new ArrayList<>();
 
+    public static void main(String[] args) {
+        MacsBook book = new MacsBook();
+        book.menu();
+    }
+
     private void menu() {
         System.out.println("Menu!");
         System.out.println("Options");
-        System.out.println("  create - add marks");
+        System.out.println("  create - reset and add marks");
         System.out.println("  all - view all student names, numbers, marks, and averages");
         System.out.println("  single - view single student info");
         System.out.println("  exit - exit application");
@@ -26,15 +26,16 @@ public class MacsBook {
         while (true) {
             System.out.print("> ");
             String line = s.nextLine().toLowerCase();
-            int displayType = 0;
+            boolean displayAll = false;
             switch (line) {
                 case "create":
                     createMenu();
-                    break InputLoop;
+                    break;
                 case "all":
+                    displayAll = true;
                 case "single":
                     marksMenu(displayAll);
-                    break InputLoop;
+                    break;
                 case "exit":
                     break InputLoop;
                 default:
@@ -42,6 +43,66 @@ public class MacsBook {
                     break;
             }
         }
+    }
+
+    private void marksMenu(boolean displayAll) {
+        Scanner s = new Scanner(System.in);
+        if (!displayAll) {
+            while (true) {
+                System.out.println("--- Choose Student");
+                System.out.println("Note: input blank line to exit prematurely.");
+                System.out.print("> ");
+                String line = s.nextLine().strip();
+                if (line.length() == 0) {
+                    break;
+                }
+                ArrayList<Integer> matches = new ArrayList<>();
+                for (int i = 0; i < names.size(); i++)
+                    if (names.get(i).equals(line))
+                        matches.add(i);
+                if (matches.size() == 0) {
+                    System.out.println("--- name not found in records");
+                    continue;
+                }
+                int chosenI = -1;
+                if (matches.size() == 1) {
+                    chosenI = matches.get(0);
+                } else {
+                    while (true) {
+                        for (int i : matches) {
+                            printRecord(i);
+                        }
+                        System.out.println("--- Multiple student with name found.");
+                        System.out.println("--- Input student index:");
+                        System.out.print("> ");
+                        chosenI = s.nextInt();
+                        if (chosenI < 0 || chosenI >= names.size()) {
+                            System.out.println("--- invalid student index given");
+                            continue;
+                        }
+                        break;
+                    }
+                }
+                printRecord(chosenI);
+                break;
+            }
+        } else {
+            System.out.printf("Printing %d records:\n", names.size());
+            for (int i = 0; i < names.size(); i++) {
+                printRecord(i);
+            }
+            System.out.printf("Class Average: %.3f\n",classAverage());
+        }
+    }
+
+    private void printRecord(int i) {
+        System.out.printf("Student Index %d\n", i);
+        System.out.printf("  Name: %s\n", names.get(i));
+        System.out.printf("  Number: %s\n", numbers.get(i));
+        System.out.printf("  Assignment Mark: %.2f\n", aMarks.get(i));
+        System.out.printf("  Test Mark: %.2f\n", tMarks.get(i));
+        System.out.printf("  Final Mark: %.2f\n", fMarks.get(i));
+        System.out.printf("  Average: %.2f\n", studentAverage(i));
     }
 
     private double studentAverage(int i) {
@@ -61,7 +122,8 @@ public class MacsBook {
         System.out.println("=== Create Data");
         System.out.println("--- Input Names");
         System.out.println("Note: input blank line to end.");
-        for (int i = 0; true; i++) {
+        int init = names.size();
+        for (int i = init; true; i++) {
             System.out.printf("%d\tname> ", i);
             String line = s.nextLine();
             if (line.length() == 0)
@@ -69,7 +131,7 @@ public class MacsBook {
             names.add(line.strip());
         }
         System.out.println("--- Input Numbers");
-        for (int i = 0; i < names.size(); i++) {
+        for (int i = init; i < names.size(); i++) {
             System.out.printf("%d\tnumber for %s> ", i, names.get(i));
             String line = s.nextLine();
             try {
@@ -80,7 +142,7 @@ public class MacsBook {
             }
         }
         System.out.println("--- Input Student Assignment Marks (AM) (percent)");
-        for (int i = 0; i < names.size(); i++) {
+        for (int i = init; i < names.size(); i++) {
             System.out.printf("%d\tAM for %s> ", i, names.get(i));
             String line = s.nextLine();
             try {
@@ -95,7 +157,7 @@ public class MacsBook {
             }
         }
         System.out.println("--- Input Student Test Marks (TM) (percent)");
-        for (int i = 0; i < names.size(); i++) {
+        for (int i = init; i < names.size(); i++) {
             System.out.printf("%d\tTM for %s> ", i, names.get(i));
             String line = s.nextLine();
             try {
@@ -110,7 +172,7 @@ public class MacsBook {
             }
         }
         System.out.println("--- Input Student Final (Project) Marks (FM) (percent)");
-        for (int i = 0; i < names.size(); i++) {
+        for (int i = initc; i < names.size(); i++) {
             System.out.printf("%d\tFM for %s> ", i, names.get(i));
             String line = s.nextLine();
             try {
