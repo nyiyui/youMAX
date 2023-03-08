96a2d9233b83bcf2d568043377d1ad85bd8aa0d6
Synamin <125604267+Synamin@users.noreply.github.com>
2023-03-08 18:15:00 -0500

    Driver class edits :D

--- src/CrazySentences/PoemVerseMain.java
+++ src/CrazySentences/PoemVerseMain.java
@@ -2,16 +2,24 @@
  * Names: Ivy Zhuang, Ken Shibata, Youmin Lee
  * Teacher: Ms. Krasteva
  * Date: March 7, 2023
- * Purpose: driver class, shows user 5 randomly generated sentences as well as
- * Contributions: the whole thing -> Ivy :D
+ * Purpose: driver class, allows the user to pick the amount of verses they would like in their poem and display it.
+ * Contributions: Ivy created the driver class and original code, Youmin added the user input scanner
+ * and changed it so that the number of verses in the poem is to the user's choice and the welcome line. Also added in the comments.
  */
 package CrazySentences;
 import java.util.Scanner;
 
 public class PoemVerseMain {
-    // TODO: make main menu for both crazy sentences and AI poem
+    /**
+     * Driver class for the PoemVerse class
+     * Introduces the program and then asks how many verses they want in their random poem.
+     * Then we take their input and put it in a loop to generate the amount of verses that they want in their poem.
+     * The output is their randomly generated poem that has the amount of verses that they wish for!
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("How many verses would you like in your poem? :D");
+        System.out.println("Welcome to the PoemVerse program! This program generates a random AI-generated poem with as many verses as you choose ;)");
+        System.out.println("How many verses would you like in your poem?");
         Scanner s = new Scanner(System.in);
         int verses = s.nextInt();
         for (int i=0;i<verses;i++){
--- src/CrazySentences/SentencesMain.java
+++ src/CrazySentences/SentencesMain.java
@@ -1,7 +1,21 @@
+/**
+ * Names: Ivy Zhuang, Ken Shibata, Youmin Lee
+ * Teacher: Ms. Krasteva
+ * Date: March 7, 2023
+ * Purpose: driver class, gives 5 randomly generated sentences that seem to make no sense but have a clear structure to them :D
+ * Contributions: Ivy created the driver class and original code, Youmin added the comments and the introduction line.
+ */
 package CrazySentences;
 
 public class SentencesMain {
+    /**
+     * Driver class for the Sentence class
+     * Introduces the program and says what it is about and gives a preparatory message for the output.
+     * The for loop loops 5 times to generate 5 random sentences and outputs them for the user to see.
+     * @param args
+     */
     public static void main(String[] args) {
+        System.out.println("Welcome to the Sentences program! This program will randomly generate 5 crazy sentences for your enjoyment!");
         System.out.println("Here are your 5 CRAZY SENTENCES!!!");
         for (int i=0;i<5;i++){
             Sentence sentence = new Sentence();
