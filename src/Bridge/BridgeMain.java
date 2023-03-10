ecd80132279805ad0b646fccc2581c5ded62fd8a
Synamin <125604267+Synamin@users.noreply.github.com>
2023-03-10 14:31:44 -0500

    Created the driver class for Bridge :D

---
+++ src/Bridge/BridgeMain.java
@@ -0,0 +1,11 @@
+package Bridge;
+
+public class BridgeMain {
+    public static void main(String[] args) {
+        // TODO: copy this main maethod into a separate driver class and add some other UI elements
+        System.out.println("Playing one round of Bridge!!");
+        Bridge round = new Bridge();
+        round.simulateRound();
+
+    }
+}
