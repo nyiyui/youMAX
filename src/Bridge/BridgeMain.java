80186e4e3a6f4e23156b6fbc7959e258ce46e6c4
Synamin <125604267+Synamin@users.noreply.github.com>
2023-03-10 23:12:05 -0500

    added user input array ;-;

--- src/Bridge/BridgeMain.java
+++ src/Bridge/BridgeMain.java
@@ -1,9 +1,30 @@
+/**
+ * Names: Ivy Zhuang, Ken Shibata, Youmin Lee
+ * Teacher: Ms. Krasteva
+ * Date: March 7, 2023
+ * Purpose: driver class, runs the Bridge class (starts a round of the card game, Bridge)
+ * Contributions: Ivy created the main method and the Bridge class.
+ * Youmin added the comments, created the driver class and added in the user input scanner
+ */
 package Bridge;
+import CrazySentences.PoemVerse;
+
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class BridgeMain {
     public static void main(String[] args) {
-        // TODO: copy this main maethod into a separate driver class and add some other UI elements
-        System.out.println("Playing one round of Bridge!!");
+        System.out.println("Welcome to the card game Bridge. Starting one round of Bridge!");
+        //System.out.println("Please enter your players' names: ");
+        Scanner n = new Scanner(System.in);
+        String[] names = new String[4];
+        System.out.println("Please enter four player names: ");
+        for(int i=0; i<4; i++)
+        {
+        //reading array elements from the user
+            names[i]=n.nextLine();
+        }
+        System.out.println(names[1]);
         Bridge round = new Bridge();
         round.simulateRound();
 
