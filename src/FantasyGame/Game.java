53af0a045629998940c6d7bcfadba2e9d98ce637
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-04-22 00:58:14 -0400

    Update Game.java

--- src/FantasyGame/Game.java
+++ src/FantasyGame/Game.java
@@ -1,5 +1,10 @@
 /**
- * top comments lmao
+ * Names: Ken Shibata & Ivy Zhuang
+ * Teacher: Ms. Krasteva
+ * Date: Feb 21, 2023
+ * Purpose: simulation of a fantasy game, modified and better than before!
+ * Improved Storyline: printed by program.
+ *
  */
 package FantasyGame;
 
@@ -11,6 +16,7 @@ package FantasyGame;
  */
 public class Game {
 
+<<<<<<< Updated upstream
 	/**
 	 * Initiates the start of game play.
 	 */
@@ -27,16 +33,39 @@ public class Game {
 			castle.exitRoom(elf, room);
 		}
 	}
+=======
+    /**
+     * Initiates the start of game play.
+     */
+    void play() {
+        gameIntro();
+        Engine.current().addSystem(Telop.current());
+        Elf elf = new Elf("Sam");
+        elf.setupEngine(Engine.current());
+        Engine.current().loop();
+        return;
+//		Castle castle = new Castle();
+//		createLevel1(castle);
+//
+//		for (int i=0; i<castle.getRoomCount(); i++)
+//		{
+//			DarkRoom room = castle.getRoom(i);
+//			castle.enterRoom(elf, room);
+//			castle.exitRoom(elf, room);
+//		}
+    }
+>>>>>>> Stashed changes
 
-	void gameIntro(){
-		System.out.println("The story so far (aka the improved storyline): ");
-		System.out.println("Once upon a time, in your typical fantasy world, an Elf from the future suddenly appears?!");
-		System.out.println("With them, they brought new high-end technology such as the EMS (Elf Movement Service)!");
-		System.out.println("In addition, the ELf is travelling through a very high-tech forest which is installed with an AQS (Air Quality Service).");
-		System.out.println("Within this forest, the Elf can fight dragons and you-unicorns.");
-		System.out.println("Our Elf may or may not be powerful to defeat these enemies, so we shall have to see how the story unfolds. IKZ!!!");
-	}
+    void gameIntro() {
+        System.out.println("The story so far (aka the improved storyline): ");
+        System.out.println("Once upon a time, in your typical fantasy world, an Elf from the future suddenly appears?!");
+        System.out.println("With them, they brought new high-end technology such as the EMS (Elf Movement Service)!");
+        System.out.println("In addition, the ELf is travelling through a very high-tech forest which is installed with an AQS (Air Quality Service).");
+        System.out.println("Within this forest, the Elf can fight dragons and you-unicorns.");
+        System.out.println("Our Elf may or may not be powerful to defeat these enemies, so we shall have to see how the story unfolds. IKZ!!!");
+    }
 
+<<<<<<< Updated upstream
 	/**
 	 * Constructs one level of the game for a character.
 	 * @param castle
@@ -52,4 +81,20 @@ public class Game {
 		room = new CastleRoom("living room", 0, true);
 		castle.addRoom(room);
 	}
+=======
+    /**
+     * Constructs one level of the game for a character.
+     * @param castle
+     */
+    void createLevel1(Castle castle) {
+        DarkRoom room = new DarkRoom("foyer", 50, true);
+        castle.addRoom(room);
+
+        room = new DarkRoom("kitchen", 100, false);
+        castle.addRoom(room);
+
+        room = new DarkRoom("living room", 0, true);
+        castle.addRoom(room);
+    }
+>>>>>>> Stashed changes
 }
