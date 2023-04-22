5e1fc968e8e61ba82e9b8fdaae14cba99d162def
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-04-22 00:58:10 -0400

    Update Main.java

--- src/FantasyGame/Main.java
+++ src/FantasyGame/Main.java
@@ -4,7 +4,7 @@
  * Date: Feb 21, 2023
  * Purpose: simulation of a fantasy game, modified and better than before!
  * Improved Storyline: printed by program.
- *
+ * Contributions: Ken - basically everything, Ivy - the game lore/story, every single damn UML.
  * Note: Telop means text on top of graphics (e.g. captions).
  */
 package FantasyGame;
