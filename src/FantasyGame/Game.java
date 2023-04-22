40d7c29c8da87fc6b1b11944dad702e8dad88dd3
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-04-22 00:31:54 -0400

    finish storyline lmao

--- src/FantasyGame/Game.java
+++ src/FantasyGame/Game.java
@@ -37,7 +37,8 @@ public class Game {
 		System.out.println("Once upon a time, in your typical fantasy world, an Elf from the future suddenly appears?!");
 		System.out.println("With them, they brought new high-end technology such as the EMS (Elf Movement Service)!");
 		System.out.println("In addition, the ELf is travelling through a very high-tech forest which is installed with an AQS (Air Quality Service).");
-		System.out.println("Within this forest, the Elf can..."); // me add more later
+		System.out.println("Within this forest, the Elf can fight dragons and you-unicorns.");
+		System.out.println("Our Elf may or may not be powerful to defeat these enemies, so we shall have to see how the story unfolds. IKZ!!!");
 	}
 
 	/**
