8efbad58a1c20f7da3a17ffac0ea8586fdb8876a
nyiyui <+@nyiyui.ca>
2023-04-22 00:20:05 -0400

    stuff

--- src/FantasyGame/Game.java
+++ src/FantasyGame/Game.java
@@ -7,10 +7,11 @@ package FantasyGame;
  * This class is responsible for running {@link Engine}'s game loop.
  */
 public class Game {
-	private static final String storySoFar ="The story so far (aka the improved storyline): "+
-		"Once upon a time, in your typical fantasy world, an Elf from the future suddenly appears?!"+
-		"With them, they brought new high-end technology such as the EMS (Elf Movement Service)!"+
-		"In addition, the ELf is travelling through a very high-tech forest which is installed with an AQS (Air Quality Service)."+
+	private static final String storySoFar ="The story so far (aka the improved storyline):"+
+            new String(new char[10])+
+		"Once upon a time, in your typical fantasy world, an Elf from the future suddenly appears?! "+
+		"With them, they brought new high-end technology such as the EMS (Elf Movement Service)! "+
+		"In addition, the ELf is travelling through a very high-tech forest which is installed with an AQS (Air Quality Service). "+
 		"Within this forest, the Elf can...";
 
     /**
@@ -22,7 +23,7 @@ public class Game {
         Engine.current().addSystem(Telop.current());
         Telop.current().setCharacter(character);
         Telop.current().show('*', "Press \"?\" and enter to view info, \"s\" to make the main character die.");
-		Engine.current().addSystem(new TelopTimer(3000, storySoFar));
+		Engine.current().addSystem(new TelopTimer(2000, storySoFar));
         character.setupEngine(Engine.current());
         Engine.current().loop();
     }
